44917b1e64850cb82a4268f7d369346465993b77
mendoncarm <125981757+mendoncarm@users.noreply.github.com>
2023-06-08 14:24:19 -0300

    Add files via upload

--- java.js
+++ java.js
@@ -7,29 +7,6 @@ const errorMessage = document.querySelector('#errorMessage')
 var spanEmail = document.querySelector('#spanEmail')
 const inputEmail = document.querySelector('#email-input')
 
-/*buttonOne.addEventListener('click', ()=>{
-    var email = spanEmail.value
-
-    if(validateEmail(email)){
-        containerOne.classList.add('opacity');
-        divMain.classList.add('animation-botao-clicado');
-
-        setTimeout(()=>{
-            divMain.style.display='none';
-            divMessage.style.display='block';
-        },2000)
-        spanEmail.innerHTML = inputEmail.value
-    }else{
-        inputEmail.classList.add('border-input')
-    }
-})
-
-
-function validateEmail(email) {
-    var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return regex.test(email);
-  }*/
-
   buttonOne.addEventListener('click', () => {
     var email = inputEmail.value;
   
@@ -40,7 +17,7 @@ function validateEmail(email) {
       setTimeout(() => {
         divMain.style.display = 'none';
         divMessage.style.display = 'block';
-        spanEmail.innerHTML = email; // Corrigido: usar a variável 'email' em vez de 'inputEmail.value'
+        spanEmail.innerHTML = email; 
       }, 2000);
     } else {
       inputEmail.style.border='0.1em solid hsla(0, 100%, 58%, 0.911)'
